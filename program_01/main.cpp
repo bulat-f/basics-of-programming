dd86f7f940eb1327c2845c9634771b73f021566b
Bulat F <write2bulat@gmail.com>
2018-10-12 18:27:15 +0300

    Initialize repository

---
+++ program_01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// Program #01
+// INPUT: side of square
+// OUTPUT: area of square
+
+using namespace std;
+
+int main()
+{
+	int a;
+
+	cout <<  "Enter side of square" << endl;
+	cin >> a;
+
+	cout << "Area of this square is " << a * a << endl;
+
+	return 0;
+}
